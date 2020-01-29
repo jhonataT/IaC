ba749cf950a1fe7832587e599fe0245b46d757f5
JhN-Te <51134324+JhN-Te@users.noreply.github.com>
2020-01-29 13:00:21 -0300

    Update grafico_4.py

--- grafico_4.py
+++ grafico_4.py
@@ -8,7 +8,7 @@ read = pd.read_csv("a2_MANCHAS.csv")
 plt.title('Número de manchas solares de Wolfer;\n', color = 'Black')
 plt.ylabel('Manchas Solares')
 read.boxplot(column = 'manchas')
-plt.grid(b = True)
+plt.grid(True)
 
 plt.savefig('quarto_grafico.png')
 
