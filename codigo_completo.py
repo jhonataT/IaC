54579a2d61f034d8c91c2d2dc1e042abf8fe9164
JhN-Te <51134324+JhN-Te@users.noreply.github.com>
2020-01-29 16:30:17 -0300

    Update codigo_completo.py

--- codigo_completo.py
+++ codigo_completo.py
@@ -1,4 +1,5 @@
     # Séries A2 (Manchas): Número de manchas solares de Wolfer; observações anuais de 1749 a 1924.
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -8,12 +9,14 @@ read = pd.read_csv("a2_MANCHAS.csv")
 
 plt.title('Número de manchas solares de Wolfer;\n observações anuais de 1749 a 1924.', color = 'Red')
 plt.plot(read['Ano'], read['manchas'], color = 'Red')
-plt.xlabel('Ano', color = 'Red')
-plt.ylabel('Manchas', color = 'Red')
+plt.xlabel('Ano'    , color = 'Red')
 plt.xticks(rotation = 90)
+plt.ylabel('Manchas(Wolfer)', color = 'Red')
+plt.yticks(rotation = 50)
 plt.grid(True)
 
-plt.savefig('primeiro_grafico.png')
+#plt.savefig('primeiro_grafico.png')
+plt.savefig("primeiro_grafico.png")
 
 plt.show()
 
@@ -25,8 +28,9 @@ read = pd.read_csv("a2_MANCHAS.csv").iloc[0:10]
 plt.title('Número de manchas solares de Wolfer;\n 10 primeiros resultados:', color = 'Green')
 plt.plot(read['Ano'], read['manchas'], color = 'Green')
 plt.xlabel('Ano', color = 'Green')
-plt.ylabel('Manchas', color = 'Green')
 plt.xticks(rotation = 90)
+plt.ylabel('Manchas(Wolfer)', color = 'Green')
+plt.yticks(rotation = 50)
 plt.grid(True)
 
 plt.savefig('segundo_grafico.png')
@@ -41,8 +45,9 @@ read = pd.read_csv("a2_MANCHAS.csv").iloc[166:]
 plt.title('Número de manchas solares de Wolfer;\n 10 ultimos resultados:', color = 'Blue')
 plt.plot(read['Ano'], read['manchas'], color = 'Blue')
 plt.xlabel('Ano', color = 'blue')
-plt.ylabel('Manchas', color = 'blue')
 plt.xticks(rotation = 90)
+plt.ylabel('Manchas(Wolfer)', color = 'blue')
+plt.yticks(rotation = 50)
 plt.grid(True)
 
 plt.savefig('terceiro_grafico.png')
@@ -55,7 +60,7 @@ plt.show()
 read = pd.read_csv("a2_MANCHAS.csv")
 
 plt.title('Número de manchas solares de Wolfer;\n', color = 'Black')
-plt.ylabel('Manchas Solares')
+plt.ylabel('Manchas(Wolfer)')
 read.boxplot(column = 'manchas')
 plt.grid(True)
 
@@ -71,8 +76,9 @@ read = pd.read_csv("a2_MANCHAS.csv").iloc[0:30]
 plt.title('Número de manchas solares de Wolfer;\n 30 primeiros resultados:', color = 'Black')
 plt.plot(read['Ano'], read['manchas'], color = 'Y')
 plt.xlabel('Ano', color = 'Black')
-plt.ylabel('Manchas', color = 'Black')
 plt.xticks(rotation = 90)
+plt.ylabel('Manchas(Wolfer)', color = 'Black')
+plt.yticks(rotation = 50)
 plt.grid(True)
 
 plt.savefig('quinto_grafico.png')
