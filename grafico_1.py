490ecdc9736dc4a41f62da07a2c976c8e1a02eb1
JhN-Te <51134324+JhN-Te@users.noreply.github.com>
2020-01-29 16:31:01 -0300

    Update grafico_1.py

--- grafico_1.py
+++ grafico_1.py
@@ -8,7 +8,7 @@ read = pd.read_csv("a2_MANCHAS.csv")
 plt.title('Número de manchas solares de Wolfer;\n observações anuais de 1749 a 1924.', color = 'Red')
 plt.plot(read['Ano'], read['manchas'], color = 'Red')
 plt.xlabel('Ano', color = 'Red')
-plt.ylabel('Manchas', color = 'Red')
+plt.ylabel('Manchas(Wolfer)', color = 'Red')
 plt.xticks(rotation = 90)
 plt.grid(True)
 
