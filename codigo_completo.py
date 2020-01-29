1aa0e25084b0f779f1efa63139836e002ca7c03f
JhN-Te <51134324+JhN-Te@users.noreply.github.com>
2020-01-29 08:46:17 -0300

    Add files via upload

---
+++ codigo_completo.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+    # PRIMEIRO GRÁFICO:    
+
+read = pd.read_csv("a2_MANCHAS.csv")
+
+plt.title('Número de manchas solares de Wolfer;\n observações anuais de 1749 a 1924.', color = 'Red')
+plt.plot(read['Ano'], read['manchas'], color = 'Red')
+plt.xlabel('Ano', color = 'Red')
+plt.ylabel('Manchas', color = 'Red')
+plt.xticks(rotation = 90)
+plt.grid(b = True)
+
+plt.savefig('primeiro_grafico.png')
+
+plt.show()
+
+
+    # SEGUNDO GRÁFICO (10 PRIMEIROS RESULTADOS):
+
+read = pd.read_csv("a2_MANCHAS.csv").iloc[0:10]
+
+plt.title('Número de manchas solares de Wolfer;\n 10 primeiros resultados:', color = 'Green')
+plt.plot(read['Ano'], read['manchas'], color = 'Green')
+plt.xlabel('Ano', color = 'Green')
+plt.ylabel('Manchas', color = 'Green')
+plt.xticks(rotation = 90)
+plt.grid(b = True)
+
+plt.savefig('segundo_grafico.png')
+
+plt.show()
+
+
+    # TERCEIRO GRÁFICO (10 ULTIMOS RESULTADOS):
+
+read = pd.read_csv("a2_MANCHAS.csv").iloc[166:]
+
+plt.title('Número de manchas solares de Wolfer;\n 10 ultimos resultados:', color = 'Blue')
+plt.plot(read['Ano'], read['manchas'], color = 'Blue')
+plt.xlabel('Ano', color = 'blue')
+plt.ylabel('Manchas', color = 'blue')
+plt.xticks(rotation = 90)
+plt.grid(b = True)
+
+plt.savefig('terceiro_grafico.png')
+
+plt.show()
+
+
+    #  BOXPLOT DOS DADOS:
+
+read = pd.read_csv("a2_MANCHAS.csv")
+
+plt.title('Número de manchas solares de Wolfer;\n', color = 'Black')
+plt.ylabel('Manchas Solares')
+read.boxplot(column = 'manchas')
+plt.grid(b = True)
+
+plt.savefig('quarto_grafico.png')
+
+plt.show()
+
+
+    # 30 primeiros resultados:
+
+read = pd.read_csv("a2_MANCHAS.csv").iloc[0:30]
+
+plt.title('Número de manchas solares de Wolfer;\n 30 primeiros resultados:', color = 'Black')
+plt.plot(read['Ano'], read['manchas'], color = 'Y')
+plt.xlabel('Ano', color = 'Black')
+plt.ylabel('Manchas', color = 'Black')
+plt.xticks(rotation = 90)
+plt.grid(b = True)
+
+plt.savefig('quinto_grafico.png')
+
+plt.show()
